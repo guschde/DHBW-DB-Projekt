8808d7491bc9c918c26d0825b808460a3d748b25
guschde <andreas.guerster@web.de>
2017-12-28 18:37:04 +0100

    DB Wechsel zu MYSQL

--- dbproject/settings.py
+++ dbproject/settings.py
@@ -79,12 +79,22 @@ WSGI_APPLICATION = 'dbproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbproject',
+        'USER': 'root',
+        'PASSWORD': 'bestesteam-wwi16',
+        'HOST': 'localhost',
+        'PORT': '80',
     }
 }
+
 
 
 # Password validation
