d3eede239201beff43e13be462195e46cfb252e1
guschde <andreas.guerster@web.de>
2018-01-18 00:43:55 +0100

    die migrations mal wieder

--- polls/models.py
+++ polls/models.py
@@ -19,6 +19,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default=1)
     def __str__(self):
         return self.choice_text
+
