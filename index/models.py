89f45e19594a8e5c5f647bfa1bf065cdd8deb939
dlbo <33659522+dlbo@users.noreply.github.com>
2018-02-06 16:34:30 +0100

    DB

--- index/models.py
+++ index/models.py
@@ -58,25 +58,6 @@ class Hilfstabelle3(models.Model):
 	Vorfall-ID = models.ForeignKey(Vorfall, on_delete=models.CASCADE)
 	Patient-ID = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=1)
-    def __str__(self):
-        return self.choice_text
-
 =======
 #class Einsatz
 >>>>>>> cf35e8f22044424357fd867b162cfdd682c7c578
