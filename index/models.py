a74825c4412b2829b13e12caabf39daa9188cc17
guschde <andreas.guerster@web.de>
2018-02-06 17:11:10 +0100

    Bugfixes

--- index/models.py
+++ index/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 
-<<<<<<< HEAD
 class Personal(models.Model):
 	Vorname = models.CharField(max_length=50)
 	Name = models.CharField(max_length=50)
 	Führungsqualifikation = models.CharField(max_length=50)
 	Bereitschaft = models.CharField(max_length=20)
 	Qualifikation = models.CharField(max_length=20)
+
+class Einsatz(models.Model):
+	Personal_ID = models.ForeignKey(Personal, on_delete=models.CASCADE)
 	
 class Dienst(models.Model):
 	Dienstdatum = models.DateTimeField('date published')
-	Personal-ID = models.ForeignKey(Personal, on_delete=models.CASCADE)
+	Personal_ID = models.ForeignKey(Personal, on_delete=models.CASCADE)
 	Einsatzdatum = models.ForeignKey(Einsatz, on_delete=models.CASCADE)
 	Einsatzbeginnzeit = models.DateTimeField('time published')
 	Telefonnummer = models.CharField(max_length=20)
 	Einsatzendezeit = models.DateTimeField('time published')
 	
-class Einsatz(models.Model):
-	Personal-ID = models.ForeignKey(Personal, on_delete=models.CASCADE)
+
 	
 class Ansprechpartner(models.Model):
-	Einsatz-ID = models.ForeignKey(Einsatz, on_delete=models.CASCADE)
+	Einsatz_ID = models.ForeignKey(Einsatz, on_delete=models.CASCADE)
 	Datum = models.DateTimeField('date published')
 	Telefonnummer = models.CharField(max_length=20)
 	Geschlecht = models.CharField(max_length=1)
@@ -41,25 +42,23 @@ class Patient(models.Model):
 	Vorname = models.CharField(max_length=20)
 	Name = models.CharField(max_length=20)
 	Alter = models.IntegerField()
-	Geschlecht = = models.CharField(max_length=1)
+	Geschlecht = models.CharField(max_length=1)
 	Triagekategorie = models.IntegerField()
 	Diagnose = models.CharField(max_length=200)
-	Patient-Einsatzdatum = models.DateTimeField('date published')
+	Patient_Einsatzdatum = models.DateTimeField('date published')
 	
 class Hilfstabelle1(models.Model):
 	Funkrufname = models.ForeignKey(Rettungsmittel, on_delete=models.CASCADE)
-	Vorfall-ID = models.ForeignKey(Vorfall, on_delete=models.CASCADE)
-	
-class Hilfstabelle2(models.Model):
-	Hilfs-ID1 = models.ForeignKey(Hilfstabelle1, on_delete=models.CASCADE)
-	Hilfs-ID3 = models.ForeignKey(Hilfstabelle3, on_delete=models.CASCADE)
+	Vorfall_ID = models.ForeignKey(Vorfall, on_delete=models.CASCADE)
 	
 class Hilfstabelle3(models.Model):
-	Vorfall-ID = models.ForeignKey(Vorfall, on_delete=models.CASCADE)
-	Patient-ID = models.ForeignKey(Patient, on_delete=models.CASCADE)
+	Vorfall_ID = models.ForeignKey(Vorfall, on_delete=models.CASCADE)
+	Patient_ID = models.ForeignKey(Patient, on_delete=models.CASCADE)
 
-=======
-#class Einsatz
->>>>>>> cf35e8f22044424357fd867b162cfdd682c7c578
+class Hilfstabelle2(models.Model):
+	Hilfs_ID1 = models.ForeignKey(Hilfstabelle1, on_delete=models.CASCADE)
+	Hilfs_ID3 = models.ForeignKey(Hilfstabelle3, on_delete=models.CASCADE)
+	
+
 
 
