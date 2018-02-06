114467cc17cecd4ed908153aae43ecec160b9030
guschde <andreas.guerster@web.de>
2018-02-06 17:32:04 +0100

    Formulare wiederhergesellt

--- index/urls.py
+++ index/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 app_name = 'index'
 urlpatterns = [
-   
+
     path('', views.IndexView.as_view(), name='index'),
-    path('formular/', views.FormView.as_view(), name='formular'),
+    path('einsatz/', views.Einsatz.as_view(), name='einsatz'),
+    path('vorfall/', views.Vorfall.as_view(), name='vorfall'),
+    path('helfer/', views.Helfer.as_view(), name='helfer'),
+    path('ansprechpartner/', views.Ansprechpartner.as_view(), name='ansprechpartner'),
 
 ]
 
