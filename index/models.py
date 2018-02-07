1e256e68aa83b48abb242516fc6f6241c38d2819
guschde <andreas.guerster@web.de>
2018-02-07 19:52:01 +0100

    Bugfix Datetime.now -> Date.today

--- index/models.py
+++ index/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-
+from datetime import date
 class Personal(models.Model):
 	Vorname = models.CharField(max_length=50)
 	Name = models.CharField(max_length=50)
@@ -12,13 +12,13 @@ class Personal(models.Model):
 
 class Einsatz(models.Model):
     Personal_ID = models.ForeignKey(Personal, on_delete=models.CASCADE)
-    Einsatzdatum = models.DateField('Einsatzdatum', default=datetime.now)
+    Einsatzdatum = models.DateField('Einsatzdatum', default=date.today)
     Einsatzinfo = models.CharField('Einsatzinfo', max_length=254, default='Einsatzinfo')
     def __str__(self):
         return self.Einsatzinfo
 
 class Dienst(models.Model):
-    Dienstdatum = models.DateField('Dienstdatum', default=datetime.now)
+    Dienstdatum = models.DateField('Dienstdatum', default=date.today)
     Personal_ID = models.ForeignKey(Personal, default="Gruppenführer", on_delete=models.DO_NOTHING)
     Einsatz_ID = models.ForeignKey(Einsatz, default="Einsatzleitung", on_delete=models.DO_NOTHING)
     Einsatzbeginnzeit = models.TimeField('Einsatzbeginn')
@@ -30,7 +30,7 @@ class Dienst(models.Model):
 
 class Ansprechpartner(models.Model):
 	Einsatz_ID = models.ForeignKey(Einsatz, on_delete=models.CASCADE)
-	Datum = models.DateTimeField('Datum')
+	Datum = models.DateField('Datum')
 	Telefonnummer = models.CharField(max_length=20)
 	Geschlecht = models.CharField(max_length=1)
 	Vorname = models.CharField(max_length=20)
@@ -51,14 +51,14 @@ class Patient(models.Model):
 	Geschlecht = models.CharField(max_length=1)
 	Triagekategorie = models.IntegerField()
 	Diagnose = models.CharField(max_length=200)
-	Patient_Einsatzdatum = models.DateTimeField('Behandlungsdatum')
+	Patient_Einsatzdatum = models.DateField('Behandlungsdatum')
 	def __str__(self):
 		return self.Vorname+' '+self.Name
 
 class Vorfall(models.Model):
 	Einsatz = models.ForeignKey(Einsatz, default='1', on_delete=models.CASCADE)
 	Dienst = models.ForeignKey(Dienst, default='1', on_delete=models.CASCADE)
-	Einsatzdatum = models.DateField('Einsatzdatum', default=datetime.now,)
+	Einsatzdatum = models.DateField('Einsatzdatum', default=date.today)
 	Einsatzort = models.CharField(max_length = 20, default='Heidenheim', editable=True)
 	Einsatzbeginn = models.TimeField('Einsatzbeginn', default=datetime.now)
 	Einsatzende = models.TimeField('Einsatzende')
